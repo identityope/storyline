f0ad0ed19b3116b852975362b115467be1fc1727
identityope <identityope@gmail.com>
2017-12-30 17:18:19 +0700

    format api group name

--- routes/api/test.js
+++ routes/api/test.js
@@ -5,10 +5,10 @@ module.exports = function(models, controllers, api){
 	/**
 	 * Author: ope
 	 *
-	 * @api {get} /test/check Check API
-	 * @apiName checkAPI
-	 * @apiDescription Health check to the API routes
-	 * @apiGroup Test
+	 * @api {get} /test/check API Health Check
+	 * @apiName apiHealthCheck
+	 * @apiDescription Health check API status to the server
+	 * @apiGroup _Test Functions
 	 * @apiVersion 1.0.0
 	 *
 	 * @apiSuccess {Object} data Result object
@@ -21,16 +21,16 @@ module.exports = function(models, controllers, api){
 	/**
 	 * Author: ope
 	 *
-	 * @api {get} /test/user_tokens/:user_id Get User Tokens by ID
+	 * @api {get} /redis/user_tokens/:user_id Get User Tokens by ID
 	 * @apiName GetUserTokensbyID
 	 * @apiDescription Get User Tokens by ID
-	 * @apiGroup Test
+	 * @apiGroup _Redis Functions
 	 * @apiVersion 1.0.0
 	 *
 	 * @apiSuccess {Object} data Result object
 	 *
 	 */
-	api.get("/test/user_tokens/:user_id", async function(req, res, auth){
+	api.get("/redis/user_tokens/:user_id", async function(req, res, auth){
 		var user_id = helper.sanitize(req.params.user_id);
 		var [err, result] = await wrap(controllers.authentication.getTokensByUserId(user_id));
 		if (err) {
